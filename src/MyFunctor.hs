8822812cb8bd6f2bd1beca736d997c56c56820f3
ktkim <kangtae49@gmail.com>
2023-03-06 08:22:04 +0000

    my data test

---
+++ src/MyFunctor.hs
@@ -0,0 +1,35 @@
+-- {-# LANGUAGE UndecidableInstances #-}
+
+module MyFunctor where
+
+-- import GHC.Num.BigNat (bigNatAdd)
+
+newtype MyList a = MyList
+  { unMyList :: [a]
+  }
+  deriving (Show)
+
+instance Functor MyList where
+  fmap f (MyList xs) = MyList (fmap f xs)
+
+instance Applicative MyList where
+  pure a = MyList [a]
+  (MyList fs) <*> (MyList xs) = MyList [f x | f <- fs, x <- xs]
+
+instance Monad MyList where
+  (MyList xs) >>= f = MyList [y | x <- xs, y <- unMyList (f x)]
+
+instance Semigroup (MyList a) where
+  (<>) (MyList xs) (MyList ys) = MyList (xs ++ ys)
+
+instance Monoid (MyList a) where
+  mempty = MyList []
+
+-- pure x = MyList [x]
+
+-- instance Applicative MyList => Monad MyList where
+--   (MyList xs) >>= f = MyList [y | x <- xs, y <- unMyList (f x)]
+
+f123 x = do
+  y <- x
+  return $ y * 2
